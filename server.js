7a6a39431866e5f9c588f3a02044db8b85338eb1
Devin Coleman <croilders48@gmail.com>
2025-04-20 20:00:08 -0700

    test

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+app.use(express.urlencoded({extended: true }))
+app.use(express.json())
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
